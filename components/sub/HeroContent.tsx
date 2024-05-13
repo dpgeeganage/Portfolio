f8bddbefa1dc34fb0353a40b9b7db1867334b55f
gedalk <dasun.geeganage@ifs.com>
2024-05-13 16:57:29 +0530

    add HeroContent

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import React from 'react'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/16/solid'
 
@@ -12,7 +14,7 @@ const HeroContent = () => {
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
             <motion.div 
             variants={slideInFromTop}
-            className='Welcome-box py-[15px] px[4px] border border-[#7042f88b] opacity-[0.9]'>
+            className='Welcome-box py-[8px] px[4px] border border-[#7042f88b] opacity-[0.9]'>
                 <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/>
                 <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
             </motion.div>
