c98e04fa32abd53a633cea30f96301ac52b1c793
gedalk <dasun.geeganage@ifs.com>
2024-06-13 13:22:30 +0530

    encryption

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,4 +1,4 @@
-import { Backend_skill, Frontend_skill, Full_stack, Other_skill } from '@/constants'
+import { Backend_skill, Frontend_skill, Full_stack, Other_skill, Skill_data } from '@/constants'
 import React from 'react'
 import SkillDataProvider from '../sub/SkillDataProvider'
 import SkillText from '../sub/SkillText'
@@ -10,6 +10,17 @@ const Skills = () => {
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20'
     style={{transform: "scale(0.9"}}>
         <SkillText />
+        <div className='flex flex-row justify-round flex-wrap mt-4 gap-5 items-center'>
+            {Skill_data.map((image, index) => (
+                <SkillDataProvider
+                key={index}
+                src={image.Image}
+                width={image.width}
+                height={image.height}
+                index={index} />
+            ))}
+        </div>
+
         <div className='flex flex-row justify-round flex-wrap mt-4 gap-5 items-center'>
             {Frontend_skill.map((image, index) => (
                 <SkillDataProvider
